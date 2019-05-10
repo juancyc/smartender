cf65a34825db4d9f23018dd20c1b81d7d1a165b9
Carlos Speranza <carlossperanza5@gmail.com>
2019-05-10 14:03:20 -0300

    empezamos de a poquito

--- SistemaEmbebido/blinkerRGB/blinkerRGB.ino
+++ SistemaEmbebido/blinkerRGB/blinkerRGB.ino
@@ -9,23 +9,7 @@ void setup()
 
 void loop()
 {
-  interval=analogRead(A0);
-  Serial.println(interval);
-  currentMillis = millis();
-  if(currentMillis - previousMillis >=interval){
-    previousMillis = millis();
-    if(flag == 0){
-   analogWrite(11,145);
-     analogWrite(10,112);
-     analogWrite(9,0);
-     flag = 1;
-    }
-    else{
-   analogWrite(11,0);
-     analogWrite(10,0);
-     analogWrite(9,0);
-     flag = 0;
-    }
-    
-  }
+  analogWrite(9,255);
+  delay(100);
+  analogWrite(9,255);
 }

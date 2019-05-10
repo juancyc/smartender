fd3dced13cc74d74b7dbff3a5cfacf6d723c8bc5
Carlos Speranza <carlossperanza5@gmail.com>
2019-05-10 14:28:02 -0300

    Le pongo delays lindos

--- SistemaEmbebido/LedRGB/LedRGB.ino
+++ SistemaEmbebido/LedRGB/LedRGB.ino
@@ -1,18 +1,30 @@
+long encendido = LOW;
+unsigned long previousMillis = 0;  
+const long interval = 1000;    
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(9,OUTPUT);
+  
+   pinMode(9,OUTPUT);
   pinMode(10,OUTPUT);
   pinMode(11,OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  analogWrite(9,205);
-  analogWrite(10,155);
-  analogWrite(11,72);
-  delay(1000);
-  analogWrite(9,20);
-  analogWrite(10,255);
-  analogWrite(11,255);
-  delay(1000);
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+
+    if (encendido == LOW) {
+      encendido = HIGH;
+      analogWrite(9,205);
+      analogWrite(10,155);
+      analogWrite(11,72);
+
+    } else {
+      encendido = LOW;
+      analogWrite(9,20);
+      analogWrite(10,255);
+      analogWrite(11,255);
+    }
+  }
 }

7869f6bddc782d22e49f4c4598f8a8bcdc983b36
Carlos Speranza <carlossperanza5@gmail.com>
2019-05-18 18:55:31 -0300

    motor y puente H basico

---
+++ SistemaEmbebido/puenteH/puenteH.ino
@@ -0,0 +1,26 @@
+int m1Izq = 4;
+int m1Der = 5;
+void setup() {
+  
+  pinMode(m1Izq,OUTPUT);
+  pinMode(m1Der,OUTPUT);
+  Serial.begin(3600);
+}
+
+void loop() {
+  digitalWrite(m1Der,127);
+  digitalWrite(m1Izq,LOW);
+  delay(1000);
+  digitalWrite(m1Der,LOW);
+  digitalWrite(m1Izq,LOW);
+  delay(1000);
+  digitalWrite(m1Der,LOW);
+  digitalWrite(m1Izq,HIGH);
+  delay(1000);
+  digitalWrite(m1Der,LOW);
+  digitalWrite(m1Izq,LOW);
+  delay(1000);
+
+  
+  
+}

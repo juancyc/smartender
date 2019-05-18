5ba01a1532f2990dfc83af7569c0851268d39eda
Carlos Speranza <carlossperanza5@gmail.com>
2019-05-18 19:35:20 -0300

    dht11

---
+++ SistemaEmbebido/DHT11/DHT11.ino
@@ -0,0 +1,42 @@
+#include <DHT.h>
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+void setup() {
+
+ Serial.begin(9600);
+ dht.begin();
+}
+
+void loop() {
+delay(5000);
+
+float h = dht.readHumidity();
+float t = dht.readTemperature();
+float f = dht.readTemperature(true);
+
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Error obteniendo los datos del sensor DHT11");
+    return;
+  }
+
+ float hif = dht.computeHeatIndex(f, h);
+
+ float hic = dht.computeHeatIndex(t, h, false);
+
+   Serial.print("Humedad: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperatura: ");
+  Serial.print(t);
+  Serial.print(" *C ");
+  Serial.print(f);
+  Serial.print(" *F\t");
+  Serial.print("Índice de calor: ");
+  Serial.print(hic);
+  Serial.print(" *C ");
+  Serial.print(hif);
+  Serial.println(" *F");
+
+}

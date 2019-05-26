5892afb0d472ff3620ddf5793626a4accfbaefd8
Carlos Speranza <carlossperanza5@gmail.com>
2019-05-26 18:45:51 -0300

    Ahora es no bloqueante para toda la vagancia

--- SistemaEmbebido/Integrando/PuenteH_LDR/PuenteH_LDR.ino
+++ SistemaEmbebido/Integrando/PuenteH_LDR/PuenteH_LDR.ino
@@ -1,8 +1,10 @@
 int m1Izq = 4;
 int m1Der = 5;
 int sentido = 0; //0 es no hay lu mono
+
+unsigned long previousMillis = 0;
+const long interval = 1000;     
 void setup() {
-  Serial.begin(9600);
   pinMode(12,INPUT);
   pinMode(m1Izq,OUTPUT);
   pinMode(m1Der,OUTPUT);
@@ -12,11 +14,14 @@ void setup() {
 
 void loop() {
 
+ unsigned long currentMillis = millis();
+   if (currentMillis - previousMillis >= interval) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
  if(digitalRead(12)==HIGH ){
-Serial.println("Mira como me muevo");
+
   //no hay lu
   if(sentido != 0){
-    
   digitalWrite(m1Der,LOW);
   digitalWrite(m1Izq,HIGH);
   delay(500);
@@ -27,7 +32,6 @@ Serial.println("Mira como me muevo");
  }
  else{
   if(sentido == 0){
-    Serial.println("Ahora para el otro lado");
     digitalWrite(m1Der,HIGH);
     digitalWrite(m1Izq,LOW);
       delay(500);
@@ -39,6 +43,6 @@ Serial.println("Mira como me muevo");
   //hay lu
   
  }
-
+   }
   delay(1000);
 }

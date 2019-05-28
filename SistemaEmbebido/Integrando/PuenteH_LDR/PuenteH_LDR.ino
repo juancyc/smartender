c6149422afd541ab97c10f3b6ca08f1b1d321a17
Carlos Speranza <carlossperanza5@gmail.com>
2019-05-28 17:53:16 -0300

    integracion de dht11, ldr, puente H y buzzer
    Faltaria el bmp y el esp

--- SistemaEmbebido/Integrando/PuenteH_LDR/PuenteH_LDR.ino
+++ SistemaEmbebido/Integrando/PuenteH_LDR/PuenteH_LDR.ino
@@ -1,13 +1,21 @@
+#include <DHT.h>
+
+#define DHTTYPE DHT11
 int m1Izq = 4;
 int m1Der = 5;
 int secando = 0; //0 es no hay lu mono
 const int pinbuzzer = 11;
+const int pinLDR = 12;
+const int DHTPin = 8;
+
 unsigned long previousMillis = 0;
-const long interval = 1000;     
+const long interval = 1000;
+DHT dht(DHTPin,DHTTYPE); 
+float hum,temp;    
 void setup() {
   Serial.begin(9600);
   pinMode(pinbuzzer,OUTPUT);
-  pinMode(12,INPUT);
+  pinMode(pinLDR,INPUT);
   pinMode(m1Izq,OUTPUT);
   pinMode(m1Der,OUTPUT);
   digitalWrite(pinbuzzer,HIGH);
@@ -15,12 +23,13 @@ void setup() {
 }
 
 void loop() {
-
+int dhtDisp = leerDHT();
  unsigned long currentMillis = millis();
    if (currentMillis - previousMillis >= interval) {
+    
     previousMillis = currentMillis;
     Serial.println("Entro");
- if(digitalRead(12)==HIGH ){
+ if(nohayLu()==HIGH && temp >25 & hum<48){
   Serial.println("No hay lu");
 
   //no hay lu
@@ -78,4 +87,25 @@ while(millis()  < tiempo){
 }
     digitalWrite(m1Der,LOW);
     digitalWrite(m1Izq,LOW);
+}
+/**
+ * Devuelve high cuando no hay lu
+ */
+int nohayLu(){
+return  digitalRead(pinLDR);
+}
+
+/**
+ * Devuelve si pudo leer o no lo que hay en el dht
+ */
+int leerDHT(){
+    hum = dht.readHumidity();
+    temp = dht.readTemperature();
+    
+  if (isnan(hum) || isnan(temp)) {
+    
+    Serial.println("Error obteniendo los datos del sensor DHT11");
+    return LOW;
+  }
+  return HIGH; 
 }

510035c0976510245889549d04d807b1ae52e1f4
germanalopez <germanalopez94@gmail.com>
2019-05-29 12:41:50 -0300

    Se agrega activity de eventos

---
+++ Android/smartender/app/src/main/java/com/example/smartender/DialogsHandler.java
@@ -0,0 +1,46 @@
+package com.example.smartender;
+
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class DialogsHandler {
+
+    public static void createSimpleDialog(Context context, String title, String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        /*builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });*/
+
+        builder.show();
+    }
+
+
+}
--- Android/smartender/app/src/main/java/com/example/smartender/MainActivity.java
+++ Android/smartender/app/src/main/java/com/example/smartender/MainActivity.java
@@ -1,68 +1,139 @@
 package com.example.smartender;
 
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TimePicker;
 
-import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btnAgregar, btnModificar, btnEliminar;
     private ListView listCalendarInfo;
-    private String items[] = new String[] {"Juan","Cami","Ger","Carlos","Pepeno"};
+    private ArrayList<Calendar> calendarlist;
+    private ArrayAdapter<Calendar> adapter;
+    private int selectedItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar_info);
+        calendarlist = new ArrayList<Calendar>();
+        adapter = new ArrayAdapter<Calendar>(this,R.layout.list_view,this.calendarlist);
+
+        calendarlist.add(new Calendar(1,"29/05/2019","15:30","futbol con pepeno"));
+        calendarlist.add(new Calendar(2,"12/06/2019","08:30","casamiento de Carlos"));
+        calendarlist.add(new Calendar(3,"22/06/2019","16:30","Veterinario para michin"));
 
         btnAgregar = (Button)findViewById(R.id.btnAgregar);
-        btnModificar = (Button)findViewById(R.id.btnModificar);
-        btnEliminar = (Button)findViewById(R.id.btnEliminar);
         listCalendarInfo = (ListView)findViewById(R.id.listCalendarInfo);
-        ArrayAdapter<String> adpter = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,items);
-        listCalendarInfo.setAdapter(adpter);
+        listCalendarInfo.setAdapter(adapter);
+        listCalendarInfo.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Calendar data = calendarlist.get(position);
+                createButtonsDialog(data);
+                return true;
+            }
+        });
 
-        btnAgregar.setOnClickListener(buttonsListeners);
-        btnModificar.setOnClickListener(buttonsListeners);
-        btnEliminar.setOnClickListener(buttonsListeners);
+        btnAgregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createCalendarInputDataDialog(null);
+            }
+        });
+
 
     }
 
-    private View.OnClickListener buttonsListeners = new View.OnClickListener(){
-        public void onClick(View v)
-        {
-            if(v == btnAgregar){
-                createSimpleDialog("Boton agregar","Salio bien");
-            }
-            if(v == btnModificar){
-                createSimpleDialog("Boton Modificar","Salio bien");
-            }
-            if(v == btnEliminar){
-                createSimpleDialog("Boton Eliminar","Salio bien");
-            }
-        }
 
-    };
-
-
-    private void createSimpleDialog(String title,String message){
+    private void createButtonsDialog(final Calendar calendarinfo){
         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        final CharSequence[] items = new CharSequence[2];
+        items[0] = "Modificar Evento";
+        items[1] = "Eliminar Evento";
 
-        builder.setTitle(title);
-        builder.setMessage(message);
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+        builder.setItems(items, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
+                if(items[which].toString().equals(items[0].toString())){
+                    int index = calendarlist.indexOf(calendarinfo);
+                    createCalendarInputDataDialog(calendarinfo);
+                }else{
+                    //Eliminar de la BD
+                    int index = calendarlist.indexOf(calendarinfo);
+                    calendarlist.remove(index);
+                    adapter.notifyDataSetChanged();
+                }
             }
         });
+
+        builder.show();
+    }
+
+    private void createCalendarInputDataDialog(final Calendar calendarinfo){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        LayoutInflater inflater = MainActivity.this.getLayoutInflater();
+        View v = inflater.inflate(R.layout.activity_calendar_inputdata,null);
+        builder.setView(v);
+        Button btnFecha = (Button) v.findViewById(R.id.btnFecha);
+        Button btnHora = (Button) v.findViewById(R.id.btnHora);
+        final EditText editTextFecha = (EditText) v.findViewById(R.id.editTextFecha);
+        final EditText editTextHora = (EditText) v.findViewById(R.id.editTextHora);
+        final EditText editTextDescripcion = (EditText) v.findViewById(R.id.editTextDescripcion);
+        editTextFecha.setEnabled(false);
+        editTextHora.setEnabled(false);
+
+        if(calendarinfo == null){
+            builder.setTitle("Agregar evento");
+            builder.setPositiveButton("Guardar", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    if((editTextFecha.getText().length() > 0) && (editTextHora.getText().length() > 0) && (editTextDescripcion.getText().length() > 0)){
+                        Calendar data = new com.example.smartender.Calendar();
+                        data.setDate(editTextFecha.getText().toString());
+                        data.setHour(editTextHora.getText().toString());
+                        data.setDescription(editTextDescripcion.getText().toString());
+                        calendarlist.add(data);
+                        adapter.notifyDataSetChanged();
+                        dialog.cancel();
+                    }else{
+                        DialogsHandler.createSimpleDialog(MainActivity.this,"Advertencia","No ha completado todos los campos necesarios para agregar un evento.");
+                    }
+
+                }
+            });
+        }else{
+            builder.setTitle("Modificar evento");
+            editTextFecha.setText(calendarinfo.getDate());
+            editTextHora.setText(calendarinfo.getHour());
+            editTextDescripcion.setText(calendarinfo.getDescription());
+            builder.setPositiveButton("Guardar", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    //Update en la BD
+                    calendarinfo.setDescription(editTextDescripcion.getText().toString());
+                    int index = calendarlist.indexOf(calendarinfo);
+                    calendarlist.remove(index);
+                    calendarlist.add(index,calendarinfo);
+                    adapter.notifyDataSetChanged();
+                    dialog.cancel();
+                }
+            });
+        }
 
         builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
             @Override
@@ -71,6 +142,56 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        btnFecha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final java.util.Calendar c = java.util.Calendar.getInstance(); // clase caledar de java, no la nuestra
+                int day,month,year;
+                day = c.get(java.util.Calendar.DAY_OF_MONTH);
+                month = c.get(java.util.Calendar.MONTH);
+                year = c.get(java.util.Calendar.YEAR);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        editTextFecha.setText(dayOfMonth+"/"+(month+1)+"/"+year);
+                        if(calendarinfo != null)
+                            calendarinfo.setDate(editTextFecha.getText().toString());
+                    }
+                }
+                        ,year,month,day);
+                datePickerDialog.getDatePicker().setMinDate(c.getTimeInMillis());
+                datePickerDialog.show();
+            }
+        });
+
+        btnHora.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final java.util.Calendar c = java.util.Calendar.getInstance(); // clase caledar de java, no la nuestra
+                final int hour,minutes;
+                hour = c.get(java.util.Calendar.HOUR_OF_DAY);
+                minutes = c.get(java.util.Calendar.MINUTE);
+
+                TimePickerDialog timePickerDialog = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        editTextHora.setText(hourOfDay+":"+minute);
+                        if(calendarinfo != null)
+                            calendarinfo.setHour(editTextHora.getText().toString());
+                    }
+                }
+                        ,hour,minutes,false);
+
+                timePickerDialog.show();
+            }
+        });
+
+
         builder.show();
     }
+
+
+
 }
